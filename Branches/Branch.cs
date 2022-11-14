e834f2cd9d8602f43966308bc79b23ba91a635d2
VytenisKaj <79807005+VytenisKaj@users.noreply.github.com>
2022-11-15 00:00:24 +0200

    Add files via upload

---
+++ Branches/Branch.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Branches
+{
+    public class Branch
+    {
+        List<Branch> branches;
+
+        public Branch()
+        {
+            branches = new List<Branch>();
+        }
+        public void AddBranch(Branch branch)
+        {
+            branches.Add(branch);
+        }
+
+        public List<Branch> GetBranches()
+        {
+            return branches;
+        }
+    }
+}
---
+++ Branches/Program.cs
@@ -0,0 +1,57 @@
+﻿
+namespace Branches
+{
+    public class Program
+    {
+        public static int CalculateDepthOfBranchObject(Branch? branch)
+        {
+            if(branch == null)
+            {
+                return 0;
+            }
+            if(branch.GetBranches().Count == 0)
+            {
+                return 1;
+            }
+            List<int> depths = new List<int>();
+            foreach(Branch b in branch.GetBranches())
+            {
+                depths.Add(CalculateDepthOfBranchObject(b));
+            }
+            return depths.OrderByDescending(x => x).ToList()[0] + 1;
+        }
+        public static void Main()
+        {
+            // Creating structure like in the example
+            Branch layer5 = new();
+
+            Branch layer4a = new();
+            Branch layer4b = new();
+            Branch layer4c = new();
+            layer4b.AddBranch(layer5);
+
+            Branch layer3a = new();
+            Branch layer3b = new();
+            Branch layer3c = new();
+            Branch layer3d = new();
+            layer3b.AddBranch(layer4a);
+            layer3c.AddBranch(layer4b);
+            layer3c.AddBranch(layer4c);
+
+            Branch layer2a = new();
+            Branch layer2b = new();
+            layer2a.AddBranch(layer3a);
+            layer2b.AddBranch(layer3b);
+            layer2b.AddBranch(layer3c);
+            layer2b.AddBranch(layer3d);
+
+            Branch layer1 = new();
+            layer1.AddBranch(layer2a);
+            layer1.AddBranch(layer2b);
+
+
+
+            Console.WriteLine($"Depth of object is {CalculateDepthOfBranchObject(layer1)}");
+        }
+    }
+}
